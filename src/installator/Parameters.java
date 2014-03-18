9a66879ef59045b6dc645d3a51e3152fd5380258
11cfif <11cfif@gmail.com>
2014-03-18 19:03:04 +0400

    Сохранены настройки среды и добавлены в гитигнор

---
+++ src/installator/Parameters.java
@@ -0,0 +1,53 @@
+package installator;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author vera
+ */
+public class Parameters {
+
+    public ArrayList<Object> parametersList;
+
+    Parameters() {
+        this.parametersList = null;
+    }
+
+    public void addParameterChoiceStage(int id) {
+        parametersList.add(id);
+    }
+
+    public void addParameterMultiChoiceStage(Integer[] m) {
+        parametersList.add(m);
+    }
+
+    public void addParameterPathStage(String s) {
+        parametersList.add(s);
+    }
+
+    public Object getParameter(int i) {
+        return parametersList.get(i);
+    }
+
+    public Integer[] getArrayParameter(int i) {
+        if (getParameter(i) instanceof Integer[]) {
+        return (Integer[]) getParameter(i);
+        }
+        else  throw new ClassCastException(" returns "+ getParameter(i).getClass() + "No Array");
+    }
+
+    public String getStringParameter(int i) {
+        if (getParameter(i) instanceof String) {
+        return (String) getParameter(i);
+        }
+        else  throw new ClassCastException(" returns "+ getParameter(i).getClass() + "No string");
+    }
+
+    public Integer getIntParameter(int i) {
+        if (getParameter(i) instanceof Integer) {
+            return (Integer) getParameter(i);
+        }
+        else  throw new ClassCastException(" returns "+ getParameter(i).getClass() + " No integer");
+    }
+}
